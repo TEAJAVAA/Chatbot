aaf1d8276ca5101d6cac3593025c71972d947b08
seoyoung0121 <veronica9468@naver.com>
2023-03-23 02:36:52 +0900

    data수정

--- frontend/screens/Chat.js
+++ frontend/screens/Chat.js
@@ -27,7 +27,7 @@ const backImage = require("../assets/backImage2.png");
 //For testing
 const cocktailImage = require("../assets/redcocktailimage.png");
 const testurl = "https://github.com/gradeten/Image/blob/main/assets/cat.png?raw=true";
-const url = "http://10.200.121.175:5001";
+const url = "http://192.168.0.5:5001";
 
 
 export default function Chat() {
