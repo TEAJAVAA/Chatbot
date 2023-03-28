20e65805eaa3e2efa83591f6e466661badfb7078
gradeten <izora01@ewhain.net>
2023-03-28 23:01:56 +0900

    여러번 보내지는 것 수정

--- frontend/screens/Detail.js
+++ frontend/screens/Detail.js
@@ -10,24 +10,17 @@ const Detail = ({ route, navigation }) => {
     // const navigation = useNavigation();
     const {name} = route.params;
 
-    // const [info, setInfo] = useState({
-    //     base: null,
-    //     taste: null,
-    //     degree: null,
-    //     recipe: null,
-    //     about: null,
-    //     glass: null,
-    //     content: null,
-    //     color: null,
-    // });
-    const [base, setBase] = useState("");
-    const [taste, setTaste] = useState("");
-    const [degree, setDegree] = useState("");
-    const [recipe, setRecipe] = useState("");
-    const [about, setAbout] = useState("");
-    const [glass, setGlass] = useState("");
-    const [content, setContent] = useState("");
-    const [color, setColor] = useState("");
+    const [info, setInfo] = useState({
+        base: null,
+        taste: null,
+        degree: null,
+        recipe: null,
+        about: null,
+        glass: null,
+        content: null,
+        color: null,
+    });
+
 
     useEffect(() => {
         const name_info = {
@@ -44,26 +37,19 @@ const Detail = ({ route, navigation }) => {
             .then((response) => response.json())
             .then((response) => {
                 // console.log(response.cocktail[0].base);
-                // setInfo({
-                //     base: response.cocktail[0].base,
-                //     taste: response.cocktail[0].sweet,
-                //     degree: response.cocktail[0].degree,
-                //     recipe: response.cocktail[0].recipe,
-                //     about: response.cocktail[0].info,
-                //     glass: "https://github.com/unul09/imageupload/blob/main/glass" + response.cocktail[0].glass + ".png?raw=true",
-                //     content: "https://github.com/unul09/imageupload/blob/main/content" + response.cocktail[0].glass + ".png?raw=true",
-                //     color: response.cocktail[0].color,
-                // });
-                setBase(response.cocktail[0].base);
-                setTaste(response.cocktail[0].sweet);
-                setDegree(response.cocktail[0].degree);
-                setRecipe(response.cocktail[0].recipe);
-                setAbout(response.cocktail[0].info);
-                setGlass("https://github.com/unul09/imageupload/blob/main/glass" + response.cocktail[0].glass + ".png?raw=true");
-                setContent("https://github.com/unul09/imageupload/blob/main/content" + response.cocktail[0].glass + ".png?raw=true");
-                setColor(response.cocktail[0].color);
+                setInfo({
+                    base: response.cocktail[0].base,
+                    taste: response.cocktail[0].sweet,
+                    degree: response.cocktail[0].degree,
+                    recipe: response.cocktail[0].recipe,
+                    about: response.cocktail[0].info,
+                    glass: "https://github.com/unul09/imageupload/blob/main/glass" + response.cocktail[0].glass + ".png?raw=true",
+                    content: "https://github.com/unul09/imageupload/blob/main/content" + response.cocktail[0].glass + ".png?raw=true",
+                    color: response.cocktail[0].color,
+                });
             });
     },
+        []
         // [info]
     );
 
@@ -83,11 +69,11 @@ const Detail = ({ route, navigation }) => {
 
             <ImageBackground source={require('../assets/color.jpeg')} style={styles.background}>
                 <View style={styles.centercontainer}>
-                    <ImageBackground source={{uri:glass || null}} 
+                    <ImageBackground source={{uri:info.glass || null}} 
                     style={styles.cocktailGlass}
                     >
-                        <Image source={{uri:content || null}} 
-                        style={{tintColor: color, width: 200, height: 190,}}
+                        <Image source={{uri:info.content || null}} 
+                        style={{tintColor: info.color, width: 200, height: 190,}}
                         >
                         </Image>
                     </ImageBackground>
@@ -95,15 +81,15 @@ const Detail = ({ route, navigation }) => {
             </ImageBackground>
 
             <Text style={styles.subtext}>설명:</Text>
-            <Text style={styles.text}>{about}</Text>
+            <Text style={styles.text}>{info.about}</Text>
             <Text style={styles.subtext}>레시피:</Text>
-            <Text style={styles.text}>{recipe}</Text>
+            <Text style={styles.text}>{info.recipe}</Text>
             <Text style={styles.subtext}>당도:</Text>
-            <Text style={styles.text}>{taste}</Text>
+            <Text style={styles.text}>{info.taste}</Text>
             <Text style={styles.subtext}>도수:</Text>
-            <Text style={styles.text}>{degree}</Text>
+            <Text style={styles.text}>{info.degree}</Text>
             <Text style={styles.subtext}>베이스:</Text>
-            <Text style={styles.text}>{base}</Text>
+            <Text style={styles.text}>{info.base}</Text>
         </View>
 
     );
